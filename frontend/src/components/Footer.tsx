82e6bdd8ba2c711556724db66567c7e993b498aa
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-10 00:41:19 +0530

    create responsive landing page

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,6 +5,10 @@ const Footer = () => {
                 <span className="text-3xl text-white font-bold tracking-tight">
                     LasiyaEats
                 </span>
+                <span className="text-white font-bold tracking-tight flex gap-4">
+                    <span>Privacy Policy</span>
+                    <span>Terms of Service</span>
+                </span>
             </div>
         </div>
     )
