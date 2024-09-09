c15ee17e9164fe3db2d329fef2d4a1ae1c99fb4a
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-09 20:06:16 +0530

    create basic header and footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className="bg-orange-500 py-10">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <span className="text-3xl text-white font-bold tracking-tight">
+                    LasiyaEats
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
