431fd684a5290d83b7a2e17d0dc624436cd8b99a
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-10 13:46:39 +0530

    create authentivated mobile nav items

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,9 +1,13 @@
-import { Menu } from "lucide-react"
+import { CircleUserRound, Menu } from "lucide-react"
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
 import { Separator } from "./ui/separator"
 import { Button } from "./ui/button"
+import { useAuth0 } from "@auth0/auth0-react"
+import MobileNavLinks from "./MobileNavLinks"
 
 const MobileNav = () => {
+    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
     return (
         <Sheet>
             <SheetTrigger>
@@ -11,16 +15,27 @@ const MobileNav = () => {
             </SheetTrigger>
             <SheetContent className="space-y-3">
                 <SheetTitle>
-                    <span>Welcome to LasiyaEats!</span>
+                    {isAuthenticated ? (
+                        <span className="flex items-center font-bold gap-2">
+                            <CircleUserRound className="text-orange-500" />
+                            {user?.email}
+                        </span>
+                    ) : (
+                        <span> Welcome to LasiyaEats!</span>
+                    )}
                 </SheetTitle>
                 <Separator />
                 <SheetDescription className="flex flex-col gap-4">
-                    <Button
-                        onClick={() => console.log("Sign Up clicked")}
-                        className="flex-1 font-bold bg-orange-500"
-                    >
-                        Log In
-                    </Button>
+                    {isAuthenticated ? (
+                        <MobileNavLinks />
+                    ) : (
+                        <Button
+                            onClick={() => loginWithRedirect()}
+                            className="flex-1 font-bold bg-orange-500"
+                        >
+                            Log In
+                        </Button>
+                    )}
                 </SheetDescription>
             </SheetContent>
         </Sheet>
