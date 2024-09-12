26821cbfbfda20405bb544d1c1bd3d5a8e487444
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-12 23:11:12 +0530

    add react query and create user request

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,24 @@ import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes.tsx'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </StrictMode>,
 )
