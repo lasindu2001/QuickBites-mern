d61181d5be4641f5b8bad501e896338899b972d9
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-13 23:48:42 +0530

    complete update user profile request

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,10 +1,12 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm"
 
 const UserProfilePage = () => {
+    const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
     return (
         <UserProfileForm
-            // onSave={updateUser}
-            // isLoading={isUpdateLoading}
+            onSave={updateUser}
+            isLoading={isUpdateLoading}
         />
     )
 }
