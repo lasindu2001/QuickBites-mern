d15aee8dd5a4b20f9e26de7343f4b6aab0aee385
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-14 23:14:13 +0530

    add validation middleware to restaurant api

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
@@ -13,6 +15,13 @@ const upload = multer({
 });
 
 // /api/my/restaurant
-router.post("/", upload.single("imageFile"), MyRestaurantController.createMyRestaurant);
+router.post(
+    "/", 
+    upload.single("imageFile"), 
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    MyRestaurantController.createMyRestaurant
+);
 
 export default router;
