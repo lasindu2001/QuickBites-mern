643bf4bd2132b0cb637a85685bb263757cf6b367
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-15 15:29:41 +0530

    create restaurant form menu section

--- frontend/src/forms/manage-restaurant-form/CuisinesSection.tsx
+++ frontend/src/forms/manage-restaurant-form/CuisinesSection.tsx
@@ -8,27 +8,27 @@ const CuisinesSection = () => {
 
     return (
         <div className="space-y-2">
-      <div>
-        <h2 className="text-2xl font-bold">Cuisines</h2>
-        <FormDescription>
-          Select the cuisines that your restaurant serves
-        </FormDescription>
-      </div>
-      <FormField
-        control={control}
-        name="cuisines"
-        render={({ field }) => (
-          <FormItem>
-            <div className="grid md:grid-cols-5 gap-1">
-              {cuisineList.map((cuisineItem) => (
-                <CuisineCheckBox cuisine={cuisineItem} field={field} />
-              ))}
+            <div>
+                <h2 className="text-2xl font-bold">Cuisines</h2>
+                <FormDescription>
+                    Select the cuisines that your restaurant serves
+                </FormDescription>
             </div>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </div>
+            <FormField
+                control={control}
+                name="cuisines"
+                render={({ field }) => (
+                    <FormItem>
+                        <div className="grid md:grid-cols-5 gap-1">
+                            {cuisineList.map((cuisineItem) => (
+                                <CuisineCheckBox cuisine={cuisineItem} field={field} />
+                            ))}
+                        </div>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+        </div>
     )
 }
 
