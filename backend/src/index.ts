b16b7836b1869594e14b893886e8f5a18c4c060b
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-19 16:23:22 +0530

    create serch restaurant api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute';
 import myRestaurantRoute from './routes/MyRestaurantRoute';
+import restaurantRoute from "./routes/RestaurantRoute";
 import { v2 as cloudinary } from 'cloudinary';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
@@ -22,6 +23,7 @@ app.use(cors());
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
     console.log('Server is running on port 7000');
