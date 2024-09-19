68da2a500b31d162d8fab84816013309dc678b60
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-20 01:11:12 +0530

    create search restaurant request

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,11 +1,23 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
     const { city } = useParams();
 
+    const { results, isLoading } = useSearchRestaurants(city);
+
     return (
         <div>
-            <span>User searched for {city}</span>
+            <span>
+                User searched for {city}{" "}
+                <span>
+                    {results?.data.map((restaurant) => (
+                        <span>
+                            found - {restaurant.restaurantName}, {restaurant.city}
+                        </span>
+                    ))}
+                </span>
+            </span>
         </div>
     )
 }
