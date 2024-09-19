202bcd891ed4cbe7aba13808d8316ba5a91a5ae0
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-19 12:41:01 +0530

    complete update restaurant request

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,15 +1,18 @@
-import { useCreateMyRestaurant, useGetMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm"
 
 const ManageRestaurantPage = () => {
     const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
     const { restaurant } = useGetMyRestaurant();
+    const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
+
+    const isEditing = !!restaurant;
 
     return (
         <ManageRestaurantForm
             restaurant={restaurant}
-            onSave={createRestaurant}
-            isLoading={isCreateLoading}
+            onSave={isEditing ? updateRestaurant : createRestaurant}
+            isLoading={isCreateLoading || isUpdateLoading}
         />
     )
 }
