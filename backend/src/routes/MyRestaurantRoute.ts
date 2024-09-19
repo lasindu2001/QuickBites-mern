593ee350cbb45825ff4c199372aade2233c8008d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-19 08:36:24 +0530

    create update my restaurant api

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -32,4 +32,13 @@ router.post(
     MyRestaurantController.createMyRestaurant
 );
 
+router.put(
+    "/",
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    MyRestaurantController.updateMyRestaurant
+);
+
 export default router;
