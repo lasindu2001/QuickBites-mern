391f139e37593855f65f9ecffabd8ebbe5d67626
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-25 14:11:10 +0530

    create get restaurant api

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -5,6 +5,12 @@ import RestaurantController from '../controllers/RestaurantController';
 const router = express.Router();
 
 router.get(
+    "/:restaurantId",
+    param("restaurantId").isString().trim().notEmpty().withMessage("RestaurantId paramenter must be a valid string"),
+    RestaurantController.getRestaurant
+);
+
+router.get(
     '/search/:city',
     param('city').isString().trim().notEmpty().withMessage('City paramenter must be a valid string'),
     RestaurantController.searchRestaurant
