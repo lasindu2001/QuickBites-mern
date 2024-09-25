2d7626c662af92e1c03268e30d6ed5fd4c4411ad
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-25 21:23:24 +0530

    complete detail page and menu items

---
+++ frontend/src/components/MenuItem.tsx
@@ -0,0 +1,21 @@
+import type { MenuItem } from "@/types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+    menuItem: MenuItem;
+};
+
+const MenuItem = ({ menuItem }: Props) => {
+    return (
+        <Card className="cursor-pointer">
+            <CardHeader>
+                <CardTitle>{menuItem.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="font-bold">
+                £{(menuItem.price / 100).toFixed(2)}
+            </CardContent>
+        </Card>
+    )
+}
+
+export default MenuItem
