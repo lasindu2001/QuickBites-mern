1db28b16734290b54e2f8fc2ef6109058ae35c06
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-26 23:13:16 +0530

    complete add to cart functioanlity

--- frontend/src/components/MenuItem.tsx
+++ frontend/src/components/MenuItem.tsx
@@ -3,11 +3,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 type Props = {
     menuItem: MenuItem;
+    addToCart: () => void;
 };
 
-const MenuItem = ({ menuItem }: Props) => {
+const MenuItem = ({ menuItem, addToCart }: Props) => {
     return (
-        <Card className="cursor-pointer">
+        <Card className="cursor-pointer" onClick={addToCart}>
             <CardHeader>
                 <CardTitle>{menuItem.name}</CardTitle>
             </CardHeader>
--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,12 +1,48 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItem from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+    _id: string;
+    name: string;
+    price: number;
+    quantity: number;
+};
 
 const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    const addToCart = (menuItem: MenuItemType) => {
+        setCartItems((prevCartItems) => {
+            const existingCartItem = prevCartItems.find((cartItem) => cartItem._id === menuItem._id);
+            let updatedCartItems;
+            if (existingCartItem) {
+                updatedCartItems = prevCartItems.map((cartItem) =>
+                    cartItem._id === menuItem._id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+                );
+            } else {
+                updatedCartItems = [
+                    ...prevCartItems,
+                    {
+                        _id: menuItem._id,
+                        name: menuItem.name,
+                        price: menuItem.price,
+                        quantity: 1,
+                    },
+                ];
+            }
+            return updatedCartItems;
+        });
+    }
 
     if (isLoading || !restaurant) {
         return "Loading...";
@@ -27,8 +63,17 @@ const DetailPage = () => {
                     {restaurant.menuItems.map((menuItem) => (
                         <MenuItem
                             menuItem={menuItem}
+                            addToCart={() => addToCart(menuItem)}
                         />
                     ))}
+                </div>
+                <div>
+                    <Card>
+                        <OrderSummary
+                            restaurant={restaurant}
+                            cartItems={cartItems}
+                        />
+                    </Card>
                 </div>
             </div>
         </div>
