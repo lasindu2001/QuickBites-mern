47201b962d6e45af677410a494d520aa5ab434a6
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-27 15:30:35 +0530

    confirm delivery details

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -2,8 +2,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import UserProfileForm, { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useGetMyUser } from "@/api/MyUserApi";
 
-const CheckoutButton = () => {
+type Props = {
+    onCheckout: (userFormData: UserFormData) => void;
+    disabled: boolean;
+};
+
+const CheckoutButton = ({ onCheckout, disabled }: Props) => {
     const {
         isAuthenticated,
         isLoading: isAuthLoading,
@@ -11,6 +19,11 @@ const CheckoutButton = () => {
     } = useAuth0();
 
     const { pathname } = useLocation();
+
+    const {
+        currentUser,
+        isLoading: isGetUserLoading
+    } = useGetMyUser();
 
     const onLogin = async () => {
         await loginWithRedirect({
@@ -28,12 +41,27 @@ const CheckoutButton = () => {
         );
     }
 
-    if (isAuthLoading) {
+    if (isAuthLoading || !currentUser) {
         return <LoadingButton />;
     }
 
     return (
-        <div>CheckoutButton</div>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button disabled={disabled} className="bg-orange-500 flex-1">
+                    Go to checkout
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[425px] md:min-w-[700px] bg-gray-50">
+                <UserProfileForm
+                    currentUser={currentUser}
+                    onSave={onCheckout}
+                    isLoading={isGetUserLoading}
+                    title="Confirm Deliery Details"
+                    buttonText="Continue to payment"
+                />
+            </DialogContent>
+        </Dialog>
     )
 }
 
--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -5,6 +5,7 @@ import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Card, CardFooter } from "@/components/ui/card";
+import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
 import { MenuItem as MenuItemType } from "@/types";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -63,6 +64,10 @@ const DetailPage = () => {
         });
     };
 
+    const onCheckout = (userFormData: UserFormData) => {
+        console.log("Checkout", userFormData);
+    };
+
     if (isLoading || !restaurant) {
         return "Loading...";
     }
@@ -94,7 +99,10 @@ const DetailPage = () => {
                             removeFromCart={removeFromCart}
                         />
                         <CardFooter>
-                            <CheckoutButton />
+                            <CheckoutButton
+                                disabled={cartItems.length === 0}
+                                onCheckout={onCheckout}
+                            />
                         </CardFooter>
                     </Card>
                 </div>
